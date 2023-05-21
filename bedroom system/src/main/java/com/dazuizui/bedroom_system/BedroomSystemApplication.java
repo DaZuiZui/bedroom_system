c021a6531229d1f9bb6b954c22b6a446cb35e281
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-05-21 22:31:26 +0800

    Add files via upload
    springboot基础项目

---
+++ bedroom system/src/main/java/com/dazuizui/bedroom_system/BedroomSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dazuizui.bedroom_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedroomSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedroomSystemApplication.class, args);
+    }
+
+}
